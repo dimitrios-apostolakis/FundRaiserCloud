a67144421d817c746d9a47c1c3c49b441c2f852c
Dimitrios Apostolakis <dapostolakis@athtech.gr>
2022-12-01 18:37:57 +0200

    Create Category and Bind Property

--- FundRaiserCloudWeb/Pages/Categories/Create.cshtml.cs
+++ FundRaiserCloudWeb/Pages/Categories/Create.cshtml.cs
@@ -1,14 +1,29 @@
+using FundRaiserCloudWeb.Data;
 using FundRaiserWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FundRaiserCloudWeb.Pages.Categories
 {
+    [BindProperties]    //global use of bind
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        //[BindProperty]    //more than 1 should be put individually
         public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()//no bind->(Category category)
+        {
+            await _db.Category.AddAsync(Category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
