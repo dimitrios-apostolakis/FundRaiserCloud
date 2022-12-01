acd25335db83151d366f75c3501ccdf538c302fc
Dimitrios Apostolakis <dapostolakis@athtech.gr>
2022-12-01 21:34:15 +0200

    Scaffold CRUD Pages + Layout

---
+++ FundRaiserCloudWeb/Pages/CategoryTemp/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FundRaiserCloudWeb.Data;
+using FundRaiserWeb.Models;
+
+namespace FundRaiserCloudWeb.Pages.CategoryTemp
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly FundRaiserCloudWeb.Data.ApplicationDbContext _context;
+
+        public DetailsModel(FundRaiserCloudWeb.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+      public Category Category { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Category == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category.FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Category = category;
+            }
+            return Page();
+        }
+    }
+}
---
+++ FundRaiserCloudWeb/Pages/CategoryTemp/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FundRaiserCloudWeb.Data;
+using FundRaiserWeb.Models;
+
+namespace FundRaiserCloudWeb.Pages.CategoryTemp
+{
+    public class IndexModel : PageModel
+    {
+        private readonly FundRaiserCloudWeb.Data.ApplicationDbContext _context;
+
+        public IndexModel(FundRaiserCloudWeb.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Category != null)
+            {
+                Category = await _context.Category.ToListAsync();
+            }
+        }
+    }
+}
