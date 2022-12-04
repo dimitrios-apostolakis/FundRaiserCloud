cef2298627fc840fa17a84baa8805a51c211d8b0
Dimitrios Apostolakis <dapostolakis@athtech.gr>
2022-12-04 16:23:16 +0200

    Project Index Page

---
+++ FundRaiserCloudWeb/Pages/Admin/Projects/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FundRaiserCloudWeb.Pages.Admin.Projects
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
