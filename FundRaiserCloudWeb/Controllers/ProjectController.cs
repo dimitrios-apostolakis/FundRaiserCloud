18216abc9db995a3d8ecbc0b3c8b63a0e0296cb8
Dimitrios Apostolakis <dapostolakis@athtech.gr>
2022-12-04 19:59:26 +0200

    Create API in Razor Project

---
+++ FundRaiserCloudWeb/Controllers/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using FundRaiser.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FundRaiserCloudWeb.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProjectController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public ProjectController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+		public IActionResult Get()
+		{
+			var projectList = _unitOfWork.Project.GetAll();
+			return Json(new { data = projectList });
+		}
+	}
+}
