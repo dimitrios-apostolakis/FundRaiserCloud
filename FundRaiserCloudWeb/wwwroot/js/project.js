c6f83a30c5a5afe39fde5128e03cae6c4c8ccfc7
Dimitrios Apostolakis <dapostolakis@athtech.gr>
2022-12-04 21:05:55 +0200

    DataTable Render Buttons

--- FundRaiserCloudWeb/wwwroot/js/project.js
+++ FundRaiserCloudWeb/wwwroot/js/project.js
@@ -9,7 +9,19 @@
             { "data": "title", "width": "25%" },
             { "data": "projectGoal", "width": "15%" },
             { "data": "days", "width": "15%" },
-            { "data": "category.name", "width": "15%" }
+            { "data": "category.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" >
+                            <a href="/Admin/MenuItems/upsert?id=${data}"  class="btn btn-success text-white mx-2">
+                            <i class="bi bi-pencil-square"></i>  </a>
+                            <a href="/Admin/MenuItems/upsert?id=${data}"  class="btn btn-danger text-white mx-2">
+                             <i class="bi bi-trash-fill"></i>  </a>
+                            </div>`
+                },
+                "width": "15%"
+            }
         ],
         "width":"100%"
     });
