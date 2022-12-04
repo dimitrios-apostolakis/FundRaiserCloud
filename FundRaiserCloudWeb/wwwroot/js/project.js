3d452638fb86d8f21675599f18985496a2835654
Dimitrios Apostolakis <dapostolakis@athtech.gr>
2022-12-04 20:57:41 +0200

    Load Datatables

--- FundRaiserCloudWeb/wwwroot/js/project.js
+++ FundRaiserCloudWeb/wwwroot/js/project.js
@@ -1,3 +1,16 @@
 ﻿$(document).ready(function () {
-    $('#DT_load').DataTable();
+    $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/project",
+            "type": "GET",
+            "datatype":"json"
+        },
+        "columns": [
+            { "data": "title", "width": "25%" },
+            { "data": "projectGoal", "width": "15%" },
+            { "data": "days", "width": "15%" },
+            { "data": "category.name", "width": "15%" }
+        ],
+        "width":"100%"
+    });
 });
