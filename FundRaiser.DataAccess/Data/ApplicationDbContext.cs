2b3ea0dc816c9b5330057dd854925f8eaf29c513
Dimitrios Apostolakis <dapostolakis@athtech.gr>
2022-12-06 11:17:42 +0200

    Add IdentityDbContext(+NuGet core.identity, identity.ui)

--- FundRaiser.DataAccess/Data/ApplicationDbContext.cs
+++ FundRaiser.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using FundRaiser.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FundRaiser.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
